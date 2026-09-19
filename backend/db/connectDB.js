446ee547222cc4cff2d9164a90e11ed8815cf1ad
tobajetex <jetawotobajetex@gmail.com>
2026-09-19 15:24:00 +0100

    feat: initialize backend architecture and database connection

---
+++ backend/db/connectDB.js
@@ -0,0 +1,12 @@
+// server/config/db.js
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`✅ MongoDB connected succesfully`);
+  } catch (err) {
+    console.error("❌ MongoDB connection error:", err.message);
+    process.exit(1);
+  }
+};
---
+++ backend/index.js
@@ -0,0 +1,29 @@
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import { connectDB } from "./db/connectDB.js";
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/api/health", (req, res) => {
+  res.json({ message: "API is running" });
+});
+
+const PORT = process.env.PORT || 5000;
+
+async function startServer() {
+  try {
+    await connectDB();
+    app.listen(PORT, () =>
+      console.log(`🚀 Server is running on http://localhost:${PORT}`),
+    );
+  } catch (err) {
+    console.error("Mongo error:", err);
+    process.exit(1);
+  }
+}
+startServer();
